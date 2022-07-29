d7a16918e34ed527f97a653320c999b75a36703d
Zao-chen <GreatZaochen@outlook.com>
2022-07-29 17:21:01 +0800

    完成

--- src/main/java/yzdreload/yzdreload/YzdReload.java
+++ src/main/java/yzdreload/yzdreload/YzdReload.java
@@ -1,13 +1,15 @@
 package yzdreload.yzdreload;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 
 public final class YzdReload extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        BukkitTask Message = new yzdreload.yzdreload.task.reload().runTaskTimer(this,0,600); //检测整点计时器
+        saveDefaultConfig();
     }
 
     @Override
---
+++ src/main/java/yzdreload/yzdreload/task/reload.java
@@ -0,0 +1,56 @@
+package yzdreload.yzdreload.task;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class reload extends BukkitRunnable {
+    @Override
+    /*新线程执行*/
+    public void run()
+    {
+        Plugin config = yzdreload.yzdreload.YzdReload.getProvidingPlugin(yzdreload.yzdreload.YzdReload.class); //获取配置文件
+        SimpleDateFormat formatter= new SimpleDateFormat("mm"); //获取时间戳并格式化
+        Date date = new Date(System.currentTimeMillis());
+        /*检测有没有重启过*/
+        if(formatter.format(date).equals("58")) //重启提示
+        {
+            Bukkit.broadcastMessage("§7§l[§6重启系统§7§l] §r服务器将在两分钟后重启，请确保你处在安全区域");
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(),"qq say 服务器将在两分钟后重启"); //q群提示
+            config.getConfig().set("have-reloaded",false); //设置为未重启过
+        }
+        else if(formatter.format(date).equals("59")) //重启提示
+        {
+            Bukkit.broadcastMessage("§7§l[§6重启系统§7§l] §r服务器将在一分钟后重启，请确保你处在安全区域");
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(),"qq say 服务器将在一分钟后重启"); //q群提示
+            config.getConfig().set("have-reloaded",false); //设置为未重启过
+        }
+        else if(formatter.format(date).equals("01"))
+        {
+            config.getConfig().set("have-reloaded",false); //设置为未重启过
+        }
+        else if(formatter.format(date).equals("02"))
+        {
+            config.getConfig().set("have-reloaded",false); //设置为未重启过
+        }
+        /*保存并重载配置文件*/
+        config.saveConfig();
+        config.reloadConfig();
+
+        if(config.getConfig().getBoolean("have-reloaded")) return;
+            /*检测分钟*/
+        else if(formatter.format(date).equals("00")) //如果发现是整点
+        {
+            Bukkit.broadcastMessage("§7§l[§6重启系统§7§l] §r服务器正在重启");
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(),"qq say 服务器正在重启"); //q群提示
+            config.getConfig().set("have-reloaded",true); //设置为已重启过
+            config.saveConfig();
+            config.reloadConfig();
+            Bukkit.reload();
+        }
+    }
+}
